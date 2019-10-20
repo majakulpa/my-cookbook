81733b059dbc75c858c90cd56ef4a2fd1019feaa
majakulpa <majaweronikakulpa@gmail.com>
2019-10-20 22:01:39 +1100

    Add changes

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,22 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Ingredients.css";
 
+const _ingredients = [
+  {
+    quantity: 1,
+    cup: "tsp",
+    name: "salt"
+  },
+  {
+    quantity: 1.5,
+    cup: "tsp",
+    name: "pepper"
+  },
+  {
+    quantity: 2,
+    cup: "l",
+    name: "water"
+  },
+  {
+    quantity: 0.5,
+    cup: "",
+    name: "Chicken"
+  }
+];
+
 function Ingredients() {
+  const [quantity, setQuantity] = useState(0);
   return (
     <div className="Ingredients">
       <h3>Ingredients</h3>
-      <ul className="IngredientsList">
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-        <li>item</li>
-      </ul>
+      <table>
+        <tr>
+          <td onClick={() => setQuantity(quantity + 1)}>{quantity}</td>
+          <td>tsp</td>
+          <td>salt</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>tbs</td>
+          <td>pepper</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td></td>
+          <td>salt</td>
+        </tr>
+        <tr>
+          <td>2.5</td>
+          <td>l</td>
+          <td>water</td>
+        </tr>
+        <tr>
+          <td>0.5</td>
+          <td></td>
+          <td>chicken</td>
+        </tr>
+      </table>
     </div>
   );
 }
